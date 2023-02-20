0bc0219e3145640591a4e7e3b44f5ccfb60664b7
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-20 10:50:09 +0530

    Initial commit

--- 07-02-23/Projects/Reverse the order of the bit of an unsigned int.c
+++ 07-02-23/Projects/Reverse the order of the bit of an unsigned int.c
@@ -1,3 +1,5 @@
+//Program to reverse the order of the bits in an unsigned int value
+
 #include<stdio.h>
 #include<string.h>
 
@@ -12,16 +14,19 @@ main ()
   printf ("Enter integer number : ");
   scanf ("%d", &n);
   printf ("You enterd : %d\n", n);
-
+  
+  //calling binary function to print binary nits of enterd number
   printf ("\nBinary Number of a Given Number is :  ");
   binary (n);
 
+  //calling reverse function to print reverse bits of binary number
   printf ("\nReverse bits of Given Number is :  ");
   reverse (n);
 
   return 0;
 }
 
+//Function to convert integer to binary
 int
 binary (int x)
 {
@@ -38,6 +43,7 @@ binary (int x)
   return 0;
 }
 
+//Function to reverse the binary bits of number
 int
 reverse (int y)
 {
