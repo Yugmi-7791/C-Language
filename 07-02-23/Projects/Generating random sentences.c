14e2a3946a73febd4e3ca112868a09f8396e48a5
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-20 10:25:17 +0530

    Initial commit

--- 07-02-23/Projects/Generating random sentences.c
+++ 07-02-23/Projects/Generating random sentences.c
@@ -1,3 +1,5 @@
+//Program for random number generation to create sentences...
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -9,20 +11,22 @@ main ()
 {
   srand (time (NULL));
   int a, b, c, d, e, f, i;
+  // array of pointers to characters
   char *article[5] = { "the", "a", "one", "some", "any" };
   char *noun[5] = { "boy", "girl", "dog", "town", "car" };
   char *verb[5] = { "drove", "jumped", "ran", "walked", "skipped" };
   char *preposition[5] = { "to", "from", "over", "under", "on" };
-
+ 
     for(i=0;i<20;i++)
     {
+      //random words generation
         a = rand() % 5;
         b = rand() % 5;
         c = rand() % 5;
         d = rand() % 5;
         e = rand() % 5;
         f = rand() % 5;
-        printf("sentence %d = %s %s %s %s %s %s.\n",i+1,article[a],noun[b],verb[c],preposition[d],article[e],noun[f]);
+        printf("sentence %d = %s %s %s %s %s %s.\n",i+1,article[a],noun[b],verb[c],preposition[d],article[e],noun[f]);//printing random words as sentence
     } 
   return 0;
 }
