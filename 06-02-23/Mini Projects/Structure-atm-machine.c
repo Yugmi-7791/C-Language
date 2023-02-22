858079cfc7bf685b651a279608e14ab1c2b4361e
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-22 13:16:08 +0530

    Initial commit

--- 06-02-23/Mini Projects/Structure-atm-machine.c
+++ 06-02-23/Mini Projects/Structure-atm-machine.c
@@ -1,6 +1,8 @@
-//ATM machine:
+//ATM Machine.
 
 #include<stdio.h>
+
+//structure for multiple users.
 struct user
 {
   int account_num;
@@ -13,14 +15,18 @@ main ()
   struct user user[4];
 
   int i, n;
+  // assigning account number and pin number to array.
   int account[4] = { 12345, 23451, 34512, 45123 }, pin[4] =
-  {
-  1234, 2341, 3412, 4123};
+  {1234, 2341, 3412, 4123};
+
+  //Taking input of account number and pin number from user.
   for (i = 0; i < 4; i++)
     {
       printf ("Enter user%d account number:", i + 1);
       scanf ("%d", &user[i].account_num);
       printf ("user%d you entered: %d\n", i + 1, user[i].account_num);
+      
+      // If account number not matched then continue.
       if (account[i] != user[i].account_num)
 	{
 	  printf ("user%d Account number not matched, Try again...\n\n",
@@ -30,6 +36,8 @@ main ()
       printf ("Enter user%d pin number:", i + 1);
       scanf ("%d", &user[i].pin_num);
       printf ("user%d you entered: %d\n", i + 1, user[i].pin_num);
+
+      // If pin number not matched then print invalid pin.
       if (pin[i] != user[i].pin_num)
 	{
 	  printf ("user%d Invalid PIN.!! \n\n", i + 1);
